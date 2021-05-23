b4d4bf1446fdbb024a2041d443acb1e34d0609a0
Gean Gonçalves <gean.a.l.c@gmail.com>
2021-05-23 18:34:41 -0400

    Resolução do exercicio 5

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio_5.js
@@ -0,0 +1,39 @@
+/* Exercicio 5- Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
+Por último, façamos com que a variável seja incrementada com o valor correspondente a cada loop;
+n = 7
+
+   *
+  * *
+ *   *
+*******
+*/
+let base = 51;
+let meiaBase = (base/2).toFixed() ;
+for (let i = meiaBase; i > 0; i -= 1) {
+  let linhaAsteriscos = '';
+  if (i > 1) {
+    for (let j = 1; j <= meiaBase; j += 1) {
+      if (j < i) {
+        linhaAsteriscos += ' ';
+      } else if (j > i) {
+        linhaAsteriscos += ' ';
+      } else {
+        linhaAsteriscos += '*';
+      }
+    }
+    for (let j = 1; j <= meiaBase; j += 1) {
+      if (j < (meiaBase - i)) {
+        linhaAsteriscos += ' ';
+      } else if (j > (meiaBase - i)) {
+        linhaAsteriscos += ' ';
+      } else {
+        linhaAsteriscos += '*';
+      }
+    }
+} else {
+  for (let j = 0; j < base; j += 1) {
+    linhaAsteriscos += '*'
+  }
+}
+  console.log(linhaAsteriscos);
+}
